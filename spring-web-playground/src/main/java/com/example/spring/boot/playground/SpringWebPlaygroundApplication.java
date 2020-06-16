e0d83ffeb29f9f48ae5979ac5298231a742581a3
joao.luzio <joao.luzio@gmail.com>
2020-06-16 14:51:08 +0100

    Restructure

---
+++ spring-web-playground/src/main/java/com/example/spring/boot/playground/SpringWebPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebPlaygroundApplication.class, args);
+	}
+
+}
