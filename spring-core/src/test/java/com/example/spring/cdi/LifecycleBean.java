4961e632c6b86b4f4639a0a7fd793f2a47640a30
joao.luzio <joao.luzio@gmail.com>
2020-06-23 20:41:08 +0100

    Examples

--- spring-core/src/test/java/com/example/spring/cdi/LifecycleBean.java
+++ spring-core/src/test/java/com/example/spring/cdi/LifecycleBean.java
@@ -1,20 +1,60 @@
 package com.example.spring.cdi;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.Lifecycle;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+@Component
 @Slf4j
-public class LifecycleBean {
+public class LifecycleBean implements InitializingBean, DisposableBean, Lifecycle {
+
+  private boolean running = false;
+
   public LifecycleBean() {
     super();
     log("new");
   }
 
-  public void init() {
-    log("init");
+  @PostConstruct
+  public void annotationInit() {
+    log("annotationInit");
   }
 
-  public void destroy() {
+  @PreDestroy
+  public void annotationDestroy() {
+    log("annotationDestroy");
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    log("afterPropertiesSet");
+  }
+
+  @Override
+  public void destroy() throws Exception {
     log("destroy");
+  }
+
+  @Override
+  public void start() {
+    log("start");
+    running = true;
+  }
+
+  @Override
+  public void stop() {
+    log("stop");
+    running = false;
+  }
+
+  @Override
+  public boolean isRunning() {
+    log("start");
+    return running;
   }
 
   protected void log(String context) {
--- spring-core/src/test/java/com/example/spring/cdi/LifecycleTest.java
+++ spring-core/src/test/java/com/example/spring/cdi/LifecycleTest.java
@@ -1,15 +1,15 @@
 package com.example.spring.cdi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
 @Slf4j
 public class LifecycleTest {
   @Autowired
-  private AnnotationLifecycleBean lifecycleBean;
+  private LifecycleBean lifecycleBean;
 
   @Test
   public void test() {
