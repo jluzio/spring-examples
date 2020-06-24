55702bc402180a10b86f59d6ad24f4d9e345fc7d
joao.luzio <joao.luzio@gmail.com>
2020-06-24 16:24:36 +0100

    Examples

---
+++ spring-core/src/main/java/com/example/spring/cdi/qualifier/TypedQualifier.java
@@ -0,0 +1,24 @@
+package com.example.spring.cdi.qualifier;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.annotation.AliasFor;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier
+public @interface TypedQualifier {
+
+  @AliasFor(annotation = Qualifier.class)
+  String value();
+
+  String type() default "";
+
+}
--- spring-core/src/main/java/com/example/spring/config/AppConfig.java
+++ spring-core/src/main/java/com/example/spring/config/AppConfig.java
@@ -2,9 +2,11 @@ package com.example.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 // @ComponentScan(basePackages="com.example.spring")
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 
   @Bean
