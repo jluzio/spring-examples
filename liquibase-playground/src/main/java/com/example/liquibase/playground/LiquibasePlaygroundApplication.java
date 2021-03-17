adef1499dfd6c8cfda86eb4920fee3c4669350f1
João Luzio <joao.luzio@gmail.com>
2021-03-17 11:27:53 +0000

    Liquibase playground

---
+++ liquibase-playground/src/main/java/com/example/liquibase/playground/LiquibasePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibase.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibasePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibasePlaygroundApplication.class, args);
+	}
+
+}
