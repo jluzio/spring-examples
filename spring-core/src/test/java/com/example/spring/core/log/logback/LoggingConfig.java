e9626cb2f921de3801add9dbfd4a5a221afd77b5
João Luzio <joao.luzio@gmail.com>
2022-04-21 15:41:20 +0100

    Examples

--- spring-core/src/test/java/com/example/spring/core/log/logback/LoggingConfig.java
+++ spring-core/src/test/java/com/example/spring/core/log/logback/LoggingConfig.java
@@ -1,16 +1,26 @@
 package com.example.spring.core.log.logback;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(ConverterConfig.class)
 public class LoggingConfig {
 
-  @Autowired
-  void initMaskConverter(MaskConverter maskConverter) {
-    MaskConverterLateInitProxy.setConverter(maskConverter);
+  @Bean
+  @ConfigurationProperties(prefix = "app.log.mask-converter")
+  MaskConverter maskConverter() {
+    return new MaskConverter();
+  }
+
+  @Configuration
+  static class ProxyConfig {
+
+    @Autowired
+    void initMaskConverter(MaskConverter maskConverter) {
+      MaskConverterLateInitProxy.setConverter(maskConverter);
+    }
   }
 
 }
--- spring-core/src/test/java/com/example/spring/core/log/logback/LoggingConfigTest.java
+++ spring-core/src/test/java/com/example/spring/core/log/logback/LoggingConfigTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.verify;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.CoreConstants;
 import java.util.Map;
-import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.slf4j.ILoggerFactory;
@@ -15,8 +14,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest(
     classes = LoggingConfig.class,
@@ -28,22 +25,6 @@ import org.springframework.context.annotation.Import;
 @Slf4j
 class LoggingConfigTest {
 
-  @Configuration
-  @Import(LoggingConfig.class)
-  static class Config {
-
-    @PostConstruct
-    void checkInit() {
-      System.out.println(System.getProperties());
-      ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
-      LoggerContext loggerContext = (LoggerContext) loggerFactory;
-      Map<String, String> ruleRegistry = (Map<String, String>) loggerContext.getObject(
-          CoreConstants.PATTERN_RULE_REGISTRY);
-//      ruleRegistry.put("mask", MaskConverterProxy.class.getName());
-      log.info("ruleRegistry: {}", ruleRegistry);
-    }
-  }
-
   @SpyBean
   MaskConverter maskConverter;
 
@@ -53,4 +34,16 @@ class LoggingConfigTest {
     log.info("Number: 1234561234561234");
     verify(maskConverter, atLeastOnce()).transform(any(), any());
   }
+
+  //  @Test
+  void checkInit() {
+    System.out.println(System.getProperties());
+    ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+    LoggerContext loggerContext = (LoggerContext) loggerFactory;
+    Map<String, String> ruleRegistry = (Map<String, String>) loggerContext.getObject(
+        CoreConstants.PATTERN_RULE_REGISTRY);
+//      ruleRegistry.put("mask", MaskConverterProxy.class.getName());
+    log.info("ruleRegistry: {}", ruleRegistry);
+  }
+
 }
