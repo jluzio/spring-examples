3b9911dc2b8caaa9779b2cb384bc2652bde8e3cd
João Luzio <joao.luzio@gmail.com>
2022-09-28 18:24:14 +0100

    Liquibase Tools

--- liquibase-playground/containers/liquibase/docker/run-liquibase.sh
+++ liquibase-playground/containers/liquibase/docker/run-liquibase.sh
@@ -3,5 +3,8 @@
 # Note: calling Docker with PowerShell to avoid an issue with volumes on Git Bash (@2022/09)
 
 lb_files=$(cygpath -aw $(dirname ${BASH_SOURCE[0]})/../target/liquibase-changelog/liquibase)
+# latest
 cmd="docker run --rm -v $lb_files/changelog:/liquibase/changelog -v $lb_files/classpath:/liquibase/classpath --add-host=bridge.docker:host-gateway liquibase/liquibase '--defaultsFile=/liquibase/changelog/liquibase.docker.properties' $1"
+# version 3.x
+#cmd="docker run --rm -v $lb_files/changelog:/liquibase/changelog -v $lb_files/classpath:/liquibase/classpath --add-host=bridge.docker:host-gateway liquibase/liquibase:3.10.3 '--defaultsFile=/liquibase/changelog/liquibase.docker.properties' $1"
 powershell -c $cmd
