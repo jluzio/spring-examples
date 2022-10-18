3f164ad1135fb42cb658bc672d1134c035e27a37
João Luzio <joao.luzio@gmail.com>
2022-10-18 20:17:47 +0100

    Examples

--- spring-core/src/main/java/com/example/spring/core/api/model/ModelPublicView.java
+++ spring-core/src/main/java/com/example/spring/core/api/model/ModelPublicView.java
@@ -4,6 +4,6 @@ import com.example.spring.core.api.model.view.Public;
 import com.fasterxml.jackson.annotation.JsonView;
 
 @JsonView(Public.class)
-public interface ModelBase {
+public interface ModelPublicView {
 
 }
