9186e4bac01b03f7f38a03b431b4bc6e0e5379af
João Luzio <joao.luzio@gmail.com>
2023-04-17 15:30:28 +0100

    Examples

--- spring-data/src/main/java/com/example/spring/data/repository/UserRepository.java
+++ spring-data/src/main/java/com/example/spring/data/repository/UserRepository.java
@@ -2,13 +2,13 @@ package com.example.spring.data.repository;
 
 import com.example.spring.data.jpa.model.User;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
-public interface UserRepository extends CrudRepository<User, Long>, JpaSpecificationExecutor<User> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
   List<User> findByName(@Param("name") String name);
 
