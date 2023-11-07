62123d50cb7a3adce3f5832ef9f27bc08d27aaaf
João Luzio <joao.luzio@gmail.com>
2023-11-07 15:24:05 +0000

    Examples

--- htmx/src/main/kotlin/com/example/spring/htmx/api/components/J2HtmlTodoController.kt
+++ htmx/src/main/kotlin/com/example/spring/htmx/api/components/J2HtmlTodoController.kt
@@ -1,10 +1,9 @@
 package com.example.spring.htmx.api.components
 
 import com.example.spring.htmx.service.TodoService
-import j2html.TagCreator.div
+import j2html.TagCreator.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
-import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -20,18 +19,59 @@ class J2HtmlTodoController {
 
   @GetMapping("", produces = [MediaType.TEXT_HTML_VALUE])
   fun todos(model: ModelMap): String {
-    model["todos"] = todoService.todos
-    // TODO: create the html
-    return div().withId("todos-div")
-      .withText("todos-div")
+    val todos = todoService.todos
+    // @formatter:off
+    return div(attrs("#todos-div"),
+      table(
+        attrs(".table.table-striped.table-sm"),
+        thead(
+          tr(
+            *arrayOf("id", "name", "description", "actions").map {
+              th(it).withClass("col")
+            }.toTypedArray()
+          )
+        ),
+        tbody(
+          *todos.map {
+            tr(
+              *listOf(
+                th(it.id).attr("scope", "row"),
+                td(it.name),
+                td(it.description),
+                td()
+              ).toTypedArray()
+            )
+              .attr("hx-get", "/components/j2html/todos/${it.id}")
+              .attr("hx-trigger", "click")
+              .attr("hx-target", "#todo-div")
+              .attr("hx-swap", "outerHTML")
+          }.toTypedArray()
+        )
+      ),
+      div(attrs("#todo-div"))
+    )
       .render();
+    // @formatter:on
   }
 
   @GetMapping("{id}", produces = [MediaType.TEXT_HTML_VALUE])
   fun todo(@PathVariable id: String, model: ModelMap): String {
-    model["todo"] = todoService.todos.find { it.id == id }
-    // TODO: create the html
-    return ""
+    val todo = todoService.todos.find { it.id == id }!!
+    // @formatter:off
+    return div(
+      attrs("#todo-div.card.row.offset-md-3.col-md-6"),
+      div(
+        attrs(".card-body"),
+        h5(todo.name).withClass("card-title"),
+        p(todo.description).withClass("card-text"),
+        button("Close")
+          .withType("button")
+          .withClass("btn btn-primary")
+          .attr("_", "on click hide #todo-div")
+      )
+    )
+      .render()
+    // @formatter:on
   }
 
 }
