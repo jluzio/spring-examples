e797c263c258795afeb536b761c05631c8f34914
João Luzio <joao.luzio@gmail.com>
2024-07-23 21:31:31 +0100

    Examples

--- messaging/kafka/src/main/java/com/example/spring/messaging/kafka/fizzbuzz/FizzBuzzStreamConfig.java
+++ messaging/kafka/src/main/java/com/example/spring/messaging/kafka/fizzbuzz/FizzBuzzStreamConfig.java
@@ -9,11 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import reactor.core.publisher.Flux;
 
 @Configuration
-@Profile("fizz-buzz")
 @Slf4j
 public class FizzBuzzStreamConfig {
 
