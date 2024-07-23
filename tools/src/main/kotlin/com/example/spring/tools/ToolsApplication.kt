b9918ca0d88dfa7e672be7323ac58b3c6cb928d5
João Luzio <joao.luzio@gmail.com>
2024-07-23 14:45:43 +0100

    Spring examples

---
+++ tools/src/main/kotlin/com/example/spring/tools/ToolsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring.tools
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToolsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToolsApplication>(*args)
+}
---
+++ tools/src/test/kotlin/com/example/spring/tools/ToolsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spring.tools
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToolsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
