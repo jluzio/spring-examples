39c26b33e5a0a1a7c84ff5b8c1a4402e96c20e73
João Luzio <joao.luzio@gmail.com>
2024-10-08 12:09:36 +0100

    Examples

--- spring-core/src/main/java/com/example/spring/core/api/webmvc/GreetingController.java
+++ spring-core/src/main/java/com/example/spring/core/api/webmvc/GreetingController.java
@@ -6,9 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+  public record Message(String message, String target) {
+
+  }
+
   @GetMapping(path = "/webmvc/hello")
   public String hello() {
     return "Hello, WebMvc Spring!";
+  }
+
+  @GetMapping(path = "/webmvc/hello-message")
+  public Message helloMessage() {
+    return new Message("Hello, WebMvc Spring!", "WebMvc Spring");
   }
 
 }
---
+++ spring-core/src/test/java/com/example/spring/core/api/QueryParamsControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.spring.core.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@WebFluxTest(controllers = QueryParamsController.class)
+class QueryParamsControllerTest {
+
+  @Autowired
+  WebTestClient webTestClient;
+
+  @Test
+  void params_default() {
+    webTestClient
+        .get().uri("/query-params?paramFoo=foo&paramBar=bar")
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody()
+        .jsonPath("$.paramFoo").isEqualTo("foo")
+        .jsonPath("$.paramBar").isEqualTo("bar");
+  }
+
+  @Test
+  void params_custom_setters() {
+    webTestClient
+        .get().uri("/query-params?paramFoo=foo&param_bar=bar")
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody()
+        .jsonPath("$.paramFoo").isEqualTo("foo")
+        .jsonPath("$.paramBar").isEqualTo("bar");
+  }
+}
