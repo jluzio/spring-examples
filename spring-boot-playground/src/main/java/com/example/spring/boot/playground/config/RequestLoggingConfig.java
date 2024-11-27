73a92046ad51d2d2be8844194e9abaf544fb695a
João Luzio <joao.luzio@gmail.com>
2024-11-27 18:52:27 +0000

    Examples

--- spring-boot-playground/src/main/java/com/example/spring/boot/playground/config/RequestLoggingConfig.java
+++ spring-boot-playground/src/main/java/com/example/spring/boot/playground/config/RequestLoggingConfig.java
@@ -1,6 +1,7 @@
 package com.example.spring.boot.playground.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,15 +12,14 @@ public class RequestLoggingConfig {
 
   @Bean
   @ConditionalOnMissingBean({CommonsRequestLoggingFilter.class})
-  @ConfigurationProperties(
-      prefix = "app.http.request-logging-filter"
-  )
+  @ConditionalOnProperty(value = "app.http.request-logging-filter.enabled", havingValue = "true")
+  @ConfigurationProperties("app.http.request-logging-filter")
   public CommonsRequestLoggingFilter commonsRequestLoggingFilter() {
     CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
     loggingFilter.setIncludeClientInfo(true);
-    loggingFilter.setIncludeQueryString(true);
-    loggingFilter.setIncludePayload(true);
     loggingFilter.setIncludeHeaders(true);
+    loggingFilter.setIncludePayload(true);
+    loggingFilter.setIncludeQueryString(true);
     return loggingFilter;
   }
 }
