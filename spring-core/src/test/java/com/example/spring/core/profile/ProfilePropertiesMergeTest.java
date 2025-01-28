3c0f591ecfed9dbd9441acb5224257415d442fd6
João Luzio <joao.luzio@gmail.com>
2025-01-28 15:37:41 +0000

    Examples

--- spring-core/src/test/java/com/example/spring/core/profile/ProfilePropertiesMergeTest.java
+++ spring-core/src/test/java/com/example/spring/core/profile/ProfilePropertiesMergeTest.java
@@ -2,8 +2,14 @@ package com.example.spring.core.profile;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +26,16 @@ import org.springframework.test.context.ActiveProfiles;
 @Log4j2
 class ProfilePropertiesMergeTest {
 
+  @Data
+  @RequiredArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  static class KeyValue {
+
+    private String key;
+    private String value;
+  }
+
   @Configuration
   @EnableConfigurationProperties
   static class Config {
@@ -29,12 +45,36 @@ class ProfilePropertiesMergeTest {
     Map<String, String> someMapBean() {
       return new HashMap<>();
     }
+
+    @Bean
+    @ConfigurationProperties("app.some-data-bean")
+    KeyValue someDataBean() {
+      return new KeyValue();
+    }
+
+    @Bean
+    @ConfigurationProperties("app.some-list-map-bean")
+    List<Map<String, String>> someListMapBean() {
+      return new ArrayList<>();
+    }
+
+    @Bean
+    @ConfigurationProperties("app.some-list-data-bean")
+    List<KeyValue> someListDataBean() {
+      return new ArrayList<>();
+    }
   }
 
   @Autowired
   Environment environment;
   @Autowired
   Map<String, String> someMapBean;
+  @Autowired
+  KeyValue someDataBean;
+  @Autowired
+  List<Map<String, String>> someListMapBean;
+  @Autowired
+  List<KeyValue> someListDataBean;
 
   @Test
   void test_map_merge() {
@@ -45,6 +85,35 @@ class ProfilePropertiesMergeTest {
             "key2", "val2.2",
             "key3", "val3"
         ));
+  }
+
+  @Test
+  void test_data_merge() {
+    log.info(someDataBean);
+    assertThat(someDataBean)
+        .isEqualTo(new KeyValue("key1", "val1.2"));
+  }
+
+  @Test
+  void test_list_map_merge() {
+    log.info(someListMapBean);
+    assertThat(someListMapBean)
+        .isEqualTo(List.of(
+                Map.of("key2", "val2.2"),
+                Map.of("key3", "val3")
+            )
+        );
+  }
+
+  @Test
+  void test_list_data_merge() {
+    log.info(someListDataBean);
+    assertThat(someListDataBean)
+        .isEqualTo(List.of(
+                new KeyValue("key2", "val2.2"),
+                new KeyValue("key3", "val3")
+            )
+        );
   }
 
 }
