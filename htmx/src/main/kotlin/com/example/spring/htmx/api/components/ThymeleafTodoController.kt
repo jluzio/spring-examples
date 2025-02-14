fa2e24133490572e0c80f4a2db2e966bf6fd3164
João Luzio <joao.luzio@gmail.com>
2025-02-14 20:09:44 +0000

    Examples

--- htmx/src/main/kotlin/com/example/spring/htmx/api/components/ThymeleafTodoController.kt
+++ htmx/src/main/kotlin/com/example/spring/htmx/api/components/ThymeleafTodoController.kt
@@ -1,12 +1,14 @@
 package com.example.spring.htmx.api.components
 
 import com.example.spring.htmx.service.TodoService
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.view.FragmentsRendering
 
 private const val COMPONENTS = "components/thymeleaf"
 
@@ -24,9 +26,21 @@ class ThymeleafTodoController {
   }
 
   @GetMapping("{id}")
-  fun todo(@PathVariable id: String, model: ModelMap): String {
+  fun todo(@PathVariable id: String, model: ModelMap) = todoFragments(id, model)
+
+  @GetMapping("{id}/default")
+  fun todoDefault(@PathVariable id: String, model: ModelMap): String {
     model["todo"] = todoService.todos.find { it.id == id }
     return "$COMPONENTS/todo"
   }
 
+  @GetMapping("{id}/fragments")
+  @HxRequest
+  fun todoFragments(@PathVariable id: String, model: ModelMap): FragmentsRendering {
+    model["todo"] = todoService.todos.find { it.id == id }
+    return FragmentsRendering
+      .with("$COMPONENTS/todo")
+      .fragment("$COMPONENTS/message")
+      .build()
+  }
 }
--- htmx/src/main/kotlin/com/example/spring/htmx/service/TodoService.kt
+++ htmx/src/main/kotlin/com/example/spring/htmx/service/TodoService.kt
@@ -2,12 +2,21 @@ package com.example.spring.htmx.service
 
 import com.example.spring.htmx.model.Todo
 import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicInteger
 
 @Service
 class TodoService {
 
+  private val counter = AtomicInteger(0)
+
   val todos = (1..10).map {
-    Todo("$it", "todo-${it}", "desc-${it}")
+    Todo("${counter.incrementAndGet()}", "todo-${it}", "desc-${it}")
   }.toMutableList()
+
+  fun add(data: Todo) {
+    val (_, name, description) = data
+    val todo = Todo(counter.incrementAndGet().toString(), name, description)
+    todos += todo
+  }
 
 }
