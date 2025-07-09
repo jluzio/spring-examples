38d76bde19764167de4320278e2f6d03582b0048
João Luzio <joao.luzio@gmail.com>
2025-07-09 14:09:25 +0100

    Examples

---
+++ spring-core/src/test/java/com/example/spring/core/environment/conditional/ConditionalInnerComponentsConfigTest.java
@@ -0,0 +1,58 @@
+package com.example.spring.core.environment.conditional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+class ConditionalInnerComponentsConfigTest {
+
+  @Import(ConditionalInnerComponentsConfig.class)
+  static class Config {
+
+  }
+
+  @Configuration
+  @ConditionalOnProperty(value = "conditionalInnerComponentsConfig", havingValue = "true")
+  static class ConditionalInnerComponentsConfig {
+
+    @RestController
+    class DebugController {
+
+      @GetMapping("/debug/hello")
+      String hello() {
+        return "hello";
+      }
+    }
+  }
+
+  @Test
+  void enabledTest() {
+    appCtxRunner(true).run(ctx -> {
+      assertThat(ctx.getBeanNamesForType(ConditionalInnerComponentsConfig.DebugController.class))
+          .isNotEmpty();
+    });
+  }
+
+  @Test
+  void disabledTest() {
+    appCtxRunner(false).run(ctx -> {
+      assertThat(ctx.getBeanNamesForType(ConditionalInnerComponentsConfig.DebugController.class))
+          .isEmpty();
+    });
+  }
+
+  ApplicationContextRunner appCtxRunner(boolean enabled) {
+    return new ApplicationContextRunner()
+        .withUserConfiguration(Config.class)
+        .withPropertyValues(
+            "conditionalInnerComponentsConfig=%s".formatted(enabled)
+        );
+  }
+
+}
